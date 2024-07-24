135f9dc49d8df64abb007b4887c36e5dbf3bfb12
Peti <alexandrpet195@gmail.com>
2024-07-24 18:59:22 +0300

    crawl single page

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello word");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("a lot of website provided");
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
+
+  console.log(`start crawl ${baseURL}`);
+  crawlPage(baseURL);
+}
+
+main();
